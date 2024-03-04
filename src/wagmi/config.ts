7cff55f77c7233a795e28d2f1d51c7c2b45ddc99
croissain <nhattho03@gmail.com>
2024-03-04 15:00:58 +0700

    update walletConnect

--- src/wagmi/config.ts
+++ src/wagmi/config.ts
@@ -2,7 +2,8 @@ import { cookieStorage, createConfig, createStorage, http } from "wagmi";
 import { blastSepolia, mainnet, sepolia } from "wagmi/chains";
 import { walletConnect } from "wagmi/connectors";
 
-const projectId = process.env.NEXT_PUBLIC_PID;
+const projectId =
+  process.env.NEXT_PUBLIC_PID || "2d946c74c39f06b482e6a8d71875a209";
 
 export const config = createConfig({
   chains: [mainnet, sepolia, blastSepolia],
